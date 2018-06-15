5e597f0777dc5a2052c2749ce69871be336560ad
dmitry.amroyan@gmail.com <dmitry.amroyan@gmail.com>
2018-06-15 16:34:26 +0300

    fix styles

--- public/js/3399_test.js
+++ public/js/3399_test.js
@@ -1,7 +1,7 @@
 //ваш id партнера
 var settings = {
     partner_id : 'f08dfd477b90159ac5cef98cebe1ee90',
-    count : 3,
+    count : 4,
     url: window.location.href.toString().substr(0, 512),
     source: 'rp_videocapcinema'
 };
