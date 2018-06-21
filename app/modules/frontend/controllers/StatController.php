f436254d85b1711feea1ff1c4c9da78b0c3e990f
dmitry.amroyan@gmail.com <dmitry.amroyan@gmail.com>
2018-06-21 12:17:13 +0300

    koeff integration

--- app/modules/frontend/controllers/StatController.php
+++ app/modules/frontend/controllers/StatController.php
@@ -7,6 +7,15 @@ use Tizer\Common\Models\StatOpenrtb;
 class StatController extends \Phalcon\Mvc\Controller
 {
 
+    public $partners = [
+        '50bd8c21bfafa6e4e962f6a948b1ef92'  => 'fan',
+        'f08dfd477b90159ac5cef98cebe1ee90'  => 'fan_3399_test',
+        '31698b6796a85f7781e6ae8227856659'  => 'megapolisonline.ru',
+        '3fbedfa6485396a0270f537c792fc525'  => 'advmaker',
+        '2aa225f1f6acc0e3159456f98de2bcd1'  => 'adbless',
+        '2f3a4fccca6406e35bcf33e92dd93135'  => 'magic',
+    ];
+
     public function indexAction()
     {
 
@@ -26,28 +35,47 @@ class StatController extends \Phalcon\Mvc\Controller
         ])->toArray();
 
         $koeff = [
-            '50bd8c21bfafa6e4e962f6a948b1ef92'  => 1,
-            'f08dfd477b90159ac5cef98cebe1ee90'  => 1,
-            '31698b6796a85f7781e6ae8227856659'  => 1,
-            '3fbedfa6485396a0270f537c792fc525'  => 1,
-            '2aa225f1f6acc0e3159456f98de2bcd1'  => 1,
-            '2f3a4fccca6406e35bcf33e92dd93135'  => 1,
+            '50bd8c21bfafa6e4e962f6a948b1ef92'  => [
+                'init'  => 0.8,
+                'money' => 0.65,
+            ],
+            'f08dfd477b90159ac5cef98cebe1ee90'  => [
+                'init'  => 1,
+                'money' => 1,
+            ],
+            '31698b6796a85f7781e6ae8227856659'  => [
+                'init'  => 1,
+                'money' => 1,
+            ],
+            '3fbedfa6485396a0270f537c792fc525'  => [
+                'init'  => 1,
+                'money' => 1,
+            ],
+            '2aa225f1f6acc0e3159456f98de2bcd1'  => [
+                'init'  => 1,
+                'money' => 1,
+            ],
+            '2f3a4fccca6406e35bcf33e92dd93135'  => [
+                'init'  => 1,
+                'money' => 1,
+            ],
         ];
 
         $result = [];
         if (count($stat) > 0) {
             foreach ($stat as $values) {
-                $date = date('Y-m-d',strtotime($values['stat_openrtb_date']));
-                $money = $values['stat_openrtb_money']*$koeff[$partner_id];
+                $date   = date('Y-m-d',strtotime($values['stat_openrtb_date']));
+                $money  = $values['stat_openrtb_money']*$koeff[$partner_id]['money'];
+                $init   = floor($values['stat_openrtb_init']*$koeff[$partner_id]['init']);
 
                 if (!isset($result[$date])) {
                     $result[$date] = [
                         'date'  => $date,
-                        'init'  => $values['stat_openrtb_init'],
+                        'init'  => $init,
                         'money' => $money,
                     ];
                 } else {
-                    $result[$date]['init']  += $values['stat_openrtb_init'];
+                    $result[$date]['init']  += $init;
                     $result[$date]['money'] += $money;
                 }
             }
